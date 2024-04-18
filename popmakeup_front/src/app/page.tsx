0a3530bde81296f5ed3992a2871dfe87873c9eba
Hikaru <hkriau.i@gmail.com>
2024-04-18 12:29:39 +0900

    0417発表分

---
+++ popmakeup_front/src/app/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import SelectYearMonth from "./components/button/SelectYearMonth";
+import ChartBar from "./components/graph/ChartBar";
+import ChartDoughnut from "./components/graph/ChartDoughnut";
+import ChartHorBar from "./components/graph/ChartHorBar";
+import Summary from "./components/graph/Summary";
+
+
+export default function Home() {
+  return (
+    <div className="flex flex-col h-screen mt-20" style={{backgroundColor:"var(--sub3)"}}>
+      <div className="flex justify-center items-center w-full mb-5">
+        <div className="select-year-month"><SelectYearMonth /></div>
+      </div>
+      <div className="flex justify-center items-center w-full">
+        <div className="chart-doughnut mx-2"><Summary /></div>
+        <div className="chart-doughnut mx-2"><ChartDoughnut /></div>
+        <div className="chart-Horbar mx-2"><ChartHorBar /></div>
+      </div>
+      <div className="flex justify-center items-center w-full mt-5 mb-60">
+        <div className="chart-bar"><ChartBar /></div>
+      </div>
+    </div>
+  );
+}
+
